67cd7c131eb0d2b63286d9575bd92df92e2bf81e
pfeiffmi <pfeiffmi@oregonstate.edu>
2025-03-13 03:37:38 -0700

    modified speed of sort method and added a run method for stacking

--- ManipulatorSystem/Code/ArmPi/Functions/ObjectOrientedArmPi/Motion.py
+++ ManipulatorSystem/Code/ArmPi/Functions/ObjectOrientedArmPi/Motion.py
@@ -13,14 +13,19 @@ import HiwonderSDK.Board as Board
 
 
 class Motion():
-    def __init__(self):
+    def __init__(self, reset_time_ms=1500):
+        # define the reset speed
+        self.reset_time_ms = reset_time_ms
         # define how far up to grab the cube
         self.cube_grab_from_bottom_cm = 1.0
+        self.cube_height_cm = 3.0
         # coordinates for colored cube placement
+        #offset = 0.5
         self.home_coordinate = {
-            "red":   (-15.5, 12.5, self.cube_grab_from_bottom_cm),
-            "green": (-15.5, 6.5,  self.cube_grab_from_bottom_cm),
-            "blue":  (-15.5, 0.5,  self.cube_grab_from_bottom_cm),
+            "red":   (-14.5, 12.5, self.cube_grab_from_bottom_cm),
+            "green": (-14.5, 6.5,  self.cube_grab_from_bottom_cm),
+            "blue":  (-14.5, 0.5,  self.cube_grab_from_bottom_cm),
+            "stack": (-14.5, -7.0, [self.cube_grab_from_bottom_cm + i*(self.cube_height_cm) for i in range(0,3)])
         }
         # set an offset for the world coordinates
         self.offset_world_xy = np.array([1.5, 0.25])
@@ -50,13 +55,13 @@ class Motion():
         self._reset()
         self.reset_position()
 
-    def reset_position(self, reset_time_ms=1500):
-        Board.setBusServoPulse(1, self.servo1 - 50, min(300, reset_time_ms))
-        Board.setBusServoPulse(2, 500, min(500, reset_time_ms))
-        self.AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, reset_time_ms)
-        time.sleep(reset_time_ms/1000)
+    def reset_position(self):
+        Board.setBusServoPulse(1, self.servo1 - 50, min(300, self.reset_time_ms))
+        Board.setBusServoPulse(2, 500, min(500, self.reset_time_ms))
+        self.AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, self.reset_time_ms)
+        time.sleep(self.reset_time_ms/1000)
 
-    def run(self, color_of_interest, position_vector, speed=1.0):
+    def run_sort(self, color_of_interest, position_vector, speed=1.0):
         # ensure that the color or position vector is not 'None'
         if((color_of_interest == "None") or (position_vector is None)):
             return
@@ -81,7 +86,7 @@ class Motion():
         servo2_angle = Transform.getAngle(world_X, world_Y, rotation_angle) #calculate gripper angle rotation
         Board.setBusServoPulse(1, self.servo1 - 280, int(500/speed))  # open claw
         Board.setBusServoPulse(2, servo2_angle, int(500/speed))
-        time.sleep(0.5)
+        time.sleep(0.5/speed)
         
         # phase 2 - move toward object, with end-effector being half the height of the block (1.5 cm high)
         servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
@@ -95,7 +100,7 @@ class Motion():
 
         # phase 3 - close the claw
         Board.setBusServoPulse(1, self.servo1, int(500/speed))  # close claw
-        time.sleep(0.8/speed)
+        time.sleep(1.2/speed)
 
         # phase 4 - raise arm up with cube in hand to 12 cm high
         Board.setBusServoPulse(2, 500, int(500/speed))
@@ -133,7 +138,7 @@ class Motion():
         )
         time.sleep(movetime_ms/1000)
         
-        # phase 8 - slowly move cube to its home position
+        # phase 8 - more slowly move cube to its home position
         servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
             coordinate_data = (color_home_X, color_home_Y, color_home_Z), 
             alpha = -90, 
@@ -157,6 +162,104 @@ class Motion():
         )
         time.sleep(movetime_ms/1000)
 
-        # phase 11 - reset the arm position to its default state
-        self.reset_position(reset_time_ms=750)
-        time.sleep(1.5)
+    def run_stack(self, stack_index, position_vector, speed=1.0):
+        # ensure that the color or position vector is not 'None'
+        if((stack_index >= 3) or (stack_index < 0) or (position_vector is None)):
+            return
+        # get the individual measures from the position vector
+        world_X, world_Y = np.array(position_vector[0]) + self.offset_world_xy
+        width, height = position_vector[1]
+        rotation_angle = position_vector[2]
+        # get stack placement coordinates
+        stack_home_X, stack_home_Y, stack_home_Z = self.home_coordinate["stack"]
+        stack_home_Z = stack_home_Z[stack_index]
+        
+        # phase 0 - move end-effector to hover 7cm above the object
+        servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
+            coordinate_data = (world_X, world_Y, 7), 
+            alpha = -90, 
+            alpha1 = -90, 
+            alpha2 = 0,
+            movetime = int(1500/speed)
+        )
+        time.sleep(movetime_ms/1000)
+
+        # phase 1 - rotate the end-effector to match the cube rotation and open the claw
+        servo2_angle = Transform.getAngle(world_X, world_Y, rotation_angle) #calculate gripper angle rotation
+        Board.setBusServoPulse(1, self.servo1 - 280, int(500/speed))  # open claw
+        Board.setBusServoPulse(2, servo2_angle, int(500/speed))
+        time.sleep(0.5/speed)
+        
+        # phase 2 - move toward object, with end-effector being half the height of the block (1.5 cm high)
+        servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
+            coordinate_data = (world_X, world_Y, self.cube_grab_from_bottom_cm), 
+            alpha = -90, 
+            alpha1 = -90, 
+            alpha2 = 0, 
+            movetime = int(1000/speed)
+        )
+        time.sleep(movetime_ms/1000)
+
+        # phase 3 - close the claw
+        Board.setBusServoPulse(1, self.servo1, int(500/speed))  # close claw
+        time.sleep(0.8/speed)
+
+        # phase 4 - raise arm up with cube in hand to 12 cm high
+        Board.setBusServoPulse(2, 500, int(500/speed))
+        servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
+            coordinate_data = (world_X, world_Y, 12), 
+            alpha = -90, 
+            alpha1 = -90, 
+            alpha2 = 0, 
+            movetime = int(1000/speed)
+        )
+        time.sleep(movetime_ms/1000)
+
+        # phase 5 - move arm with cube 12 cm above the color's home position
+        servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
+            coordinate_data = (stack_home_X, stack_home_Y, 12), 
+            alpha = -90, 
+            alpha1 = -90, 
+            alpha2 = 0,
+            movetime = int(1500/speed)
+        )
+        time.sleep(movetime_ms/1000)
+        
+        # phase 6 - adjust the claw's hand angle to line up with the home base
+        servo2_angle = Transform.getAngle(stack_home_X, stack_home_Y, -90)
+        Board.setBusServoPulse(2, servo2_angle, int(500/speed))
+        time.sleep(0.5/speed)
+
+        # phase 7 - move arm 3 inches above normal cube home postion
+        servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
+            coordinate_data = (stack_home_X, stack_home_Y, stack_home_Z+3), 
+            alpha = -90, 
+            alpha1 = -90, 
+            alpha2 = 0, 
+            movetime = int(500/speed)
+        )
+        time.sleep(movetime_ms/1000)
+        
+        # phase 8 - more slowly move cube to its home position
+        servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
+            coordinate_data = (stack_home_X, stack_home_Y, stack_home_Z), 
+            alpha = -90, 
+            alpha1 = -90, 
+            alpha2 = 0, 
+            movetime = int(1000/speed)
+        )
+        time.sleep(movetime_ms/1000)
+
+        # phase 9 - open the claw
+        Board.setBusServoPulse(1, self.servo1 - 200, int(500/speed))
+        time.sleep(0.8/speed)
+
+        # phase 10 - move to 12 cm above home position
+        servos, alpha, movetime_ms = self.AK.setPitchRangeMoving(
+            coordinate_data = (stack_home_X, stack_home_Y, 12), 
+            alpha = -90, 
+            alpha1 = -90, 
+            alpha2 = 0, 
+            movetime = int(800/speed)
+        )
+        time.sleep(movetime_ms/1000)
--- ManipulatorSystem/Code/ArmPi/Functions/ObjectOrientedArmPi/main.py
+++ ManipulatorSystem/Code/ArmPi/Functions/ObjectOrientedArmPi/main.py
@@ -6,12 +6,12 @@ import time
 from Perception import Perception
 from Motion import Motion
 
-def main():
+def main(stacking):
     # Start the perception object
     perception = Perception()
     perception.start()
     # Start the motion object
-    motion = Motion()
+    motion = Motion(reset_time_ms=650)
     motion.start()
 
     # Continuous loop
@@ -23,10 +23,30 @@ def main():
             # stall a bit to not use resources
             time.sleep(0.01)
             continue
-        # move the arm to sort the colors
-        for color in position_dictionary.keys():
+        # If there are no objects in the field, then reset the robot and wait
+        if(len(position_dictionary) == 0):
+            motion.reset_position()
+            continue
+        # code to run when stacking
+        if(stacking):
+            # Get the stack index from the number of objects on the field
+            stack_index = 3 - len(position_dictionary)
+            # Get the first instance in the color-position dictionary to check the status of the field again
+            color = list(position_dictionary.keys())[0]
+            # move the arm to stack the colors
             print(f"Getting {color}")
-            motion.run(color, position_dictionary[color], speed=3.5)
+            motion.run_stack(stack_index, position_dictionary[color], speed=3.5)
+        # code to run when sorting
+        else:
+            # Get the stack index from the number of objects on the field
+            stack_index = 3 - len(position_dictionary)
+            # Get the first instance in the color-position dictionary to check the status of the field again
+            color = list(position_dictionary.keys())[0]
+            # move the arm to stack the colors
+            print(f"Getting {color}")
+            motion.run_sort(color, position_dictionary[color], speed=5.0)
+
 
 if(__name__ == "__main__"):
-    main()
+    # run main with stacking option; False=sorting
+    main(stacking=True)
